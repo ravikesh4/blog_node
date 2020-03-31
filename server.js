3dc304d0966f8fcc11e81b2d74b4244ba58eba3f
Ravikesh Yadav <ravikesh@netzary.com>
2020-03-31 17:31:46 +0530

    sign api is connected with database

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+// bringing routes 
+const blogRoutes = require('./routes/blog')
+const authRoutes = require('./routes/auth')
+
+const mongoose = require('mongoose');
+
+// db connection 
+mongoose.connect(process.env.DATABASE, { useUnifiedTopology: true,  useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+        .then(() => console.log('DB Connected'))
+
+// app 
+const app = express()
+
+// middleware 
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+// cors 
+if(process.env.NODE_ENV == 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}`}));
+}
+
+// routes middleware 
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
+
+// port 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`app listening on port ${port}`))
